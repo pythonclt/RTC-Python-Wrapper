6548ca77a1afd85b6f1fd62859a13e655e335f8d
genos <graham.enos@gmail.com>
2011-08-23 19:06:06 -0400

    PEP8 Compliance for TestRTC.py
    Just some house cleaning, really; I ran the pep8 script on TestRTC.py
    and fixed whatever it complained about.  -- Graham

--- TestRTC.py
+++ TestRTC.py
@@ -1,32 +1,35 @@
 import unittest2 as unittest
-except
-import unittest
+except:
+    import unittest
 
 import RTC
 
+
 class SimplisticTest(unittest.TestCase):
 
-	def test(self):
-		self.failUnless(True)
-	
+    def test(self):
+        self.failUnless(True)
+
+
 class TestPreReqs(unittest.TestCase):
 
     def setUp(self):
         self.numbers = [23, 55, 4, 32]
 
     def test_expected(self):
-		self.assertEqual(self.numbers[1]-self.numbers[0], self.numbers[3])
+        self.assertEqual(self.numbers[1] - self.numbers[0], self.numbers[3])
+
 
 class TestRTC(unittest.TestCase):
 
-	def setUp(self):
-		RTC.API_KEY = 'c448541518f24d79b652ccc57b384815'
-		self.bill_id = 'hr2-112'
-		self.bills = RTC.Bill.get_mult_bills([self.bill_id], sections='')
-	
-	def test_bill_id(self):
-			for i in self.bills:
-				self.assertEqual(i.bill_id, self.bill_id)
+    def setUp(self):
+        RTC.API_KEY = 'c448541518f24d79b652ccc57b384815'
+        self.bill_id = 'hr2-112'
+        self.bills = RTC.Bill.get_mult_bills([self.bill_id], sections='')
+
+    def test_bill_id(self):
+        for i in self.bills:
+            self.assertEqual(i.bill_id, self.bill_id)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
