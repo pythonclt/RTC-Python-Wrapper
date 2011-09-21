761bd337b42e2a4f437cb93c0f44152550437930
Matthew Johnson <johnson.matthew.h@gmail.com>
2011-09-21 14:18:53 -0300

    edited functions so that they don't use the dot notation

--- tests/RTCtest.py
+++ tests/RTCtest.py
@@ -12,26 +12,26 @@ RTC.apikey = 'c448541518f24d79b652ccc57b384815'
 #bill tests
 def bill():
     bill = RTC.Bill.get_bill(bill_id)
-    print bill.sponsor_id, bill.vetoed, bill.last_action.text
+    print bill['sponsor_id'], bill['vetoed'], bill['last_action']['text']
 
 
 def mult_bills():
 	bills = RTC.Bill.get_mult_bills(['hr1-112', 'hr2-112', 'hr3-112'], sections=('actions','bill_id',))
 	for i in bills:
-		for a in i.actions:
-			print a.type, a.acted_at, a.text
+		for a in i['actions']:
+			print a['type'], a['acted_at'], a['text']
 
 def bill_actions():
     actions = RTC.Bill.actions(bill_id)
     for a in actions:
-        print a.type, a.acted_at, a.text
+        print a['type'], a['acted_at'], a['text']
 
 
 def bill_passage_votes():
     passage_votes = RTC.Bill.passage_votes(bill_id)
     for i in passage_votes:
-        print i.result, i.passage_type, i.voted_at, i.text, i.how, i.roll_id,\
-            i.chamber
+        print i['result'], i['passage_type'], i['voted_at'], i['text'], i['how'], i['roll_id'],\
+            i['chamber']
 
 
 def bill_committees():  # Does not work!
@@ -42,7 +42,7 @@ def bill_committees():  # Does not work!
 def bill_titles():
     titles = RTC.Bill.titles(bill_id)
     for i in titles:
-        print i.title, i.type, i.type_as
+        print i['title'], i['type'], i['type_as']
 
 
 def bill_amendments():
@@ -65,9 +65,9 @@ def bill_related_bills():
 def bill_cosponsors():
     cosponsors = RTC.Bill.cosponsors(bill_id)
     for i in cosponsors:
-        print i.district, i.bioguide_id, i.govtrack_id, i.last_name,\
-            i.name_suffix, i.party, i.first_name, i.state, i.chamber,\
-            i.title, i.nickname
+        print i['district'], i['bioguide_id'], i['govtrack_id'], i['last_name'],\
+            i['name_suffix'], i['party'], i['first_name'], i['state'], i['chamber'],\
+            i['title'], i['nickname']
 
 
 # votes tests
@@ -104,29 +104,29 @@ def todays_floor_updates():
 # video tests
 def video_test():
     result = RTC.Videos.get_by_bill(bill_id)
-    for v in result.videos:
+    for v in result['videos']:
         print "=== Legislative_Day:  %s | Video ID: %s  | Duration: %s" %\
-            (v.legislative_day, v.clip_id, v.duration)
+            (v['legislative_day'], v['clip_id'], v['duration'])
         print "This is the video URL for the video in the mp4 format: %s" %\
-            v.clip_urls.hls
-        for clip in v.clips:
+            v['clip_urls']['hls']
+        for clip in v['clips']:
             print "* offset: %s | duration: %s | summary: %s  " %\
-                (clip.offset, clip.duration, clip.events[0])
+                (clip['offset'], clip['duration'], clip['events'][0])
 
 
-#bill()
-#mult_bills()
-#bill_actions()
-#bill_passage_votes()
-#bill_committees()
-#bill_titles()
-#bill_amendments()
-#bill_related_bills()
-#bill_cosponsors()
+bill()
+mult_bills()
+bill_actions()
+bill_passage_votes()
+bill_committees()
+bill_titles()
+bill_amendments()
+bill_related_bills()
+bill_cosponsors()
 
-#votes()
+votes()
 floor_updates()
 floor_updates_search()
-#get_mult_floor_updates()
-#todays_floor_updates()
-#video_test()
+get_mult_floor_updates()
+todays_floor_updates()
+video_test()
