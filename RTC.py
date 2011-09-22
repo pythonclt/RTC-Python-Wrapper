61297095a3cd64be0f202c330c12f033febef953
Lateef Jackson <lateef.jackson@gmail.com>
2011-09-22 10:21:00 -0400

    Filter out clips based on bill_id and bioguide_id

--- RTC.py
+++ RTC.py
@@ -326,7 +326,7 @@ class FloorUpdates(RTC_Client):
         result = super(FloorUpdates, cls)._apicall(endpoint, sections, make_obj, **params)
         return result['floor_updates']
         
-class Videos(RTC_Client):
+class HouseVideos(RTC_Client):
     """ Currently only supports house type videos """
     __help__ = RTC_helpers.VIDEO_HELPER
 
@@ -337,27 +337,29 @@ class Videos(RTC_Client):
     def get_by_bill(cls, bill_id, make_obj=False, sections=('clip_urls', 'duration',
                                              'legislative_day', 'clip_id',
                                              'video_id', 'bills', 'clips')):
-        """
-
-        """
         endpoint = "videos.json"
         params = {'clips.bills': bill_id}
-        results = super(Videos, cls)._apicall(endpoint, sections, make_obj, **params)
+        results = super(HouseVideos, cls)._apicall(endpoint, sections, make_obj, **params)
         ### Only include clips from each video that contain bill_id ###
         videos = []
         for v in results['videos']:
             if v.has_key('bills') and bill_id in v['bills']:
-                videos.append(v)
-        #################################################
+                clips = []
+                for c in v['clips']:
+                    if c.has_key('bills') and bill_id in c['bills']:
+                        clips.append(c)
+                if len(clips) > 0:
+                    v['clips'] = clips 
+                    videos.append(v)
         return videos
 
     @classmethod
-    def get_legislator_name(cls, name, make_obj=False, sections=('clip_urls', 'duration',
+    def get_by_legislator_name(cls, name, make_obj=False, sections=('clip_urls', 'duration',
                                                            'legislative_day', 'clip_id',
                                                            'video_id', 'bills', 'clips')):
         endpoint = "videos.json"
         params = {'clips.legislator_names': name}
-        results = super(Videos, cls)._apicall(endpoint, sections, make_obj, **params)
+        results = super(HouseVideos, cls)._apicall(endpoint, sections, make_obj, **params)
         ### Only include clips from each video that contain a legistrators name
         # Make sure results set has the legislators name sin it
         videos = []
@@ -374,7 +376,32 @@ class Videos(RTC_Client):
                 v['clips'] = clips
                 videos.append(v)
 
-        #################################################
+        return videos
+
+
+    @classmethod
+    def get_by_bioguide_id(cls, bioguide_id, make_obj=False, sections=('clip_urls', 'duration',
+                                                           'legislative_day', 'clip_id',
+                                                           'video_id', 'bills', 'clips')):
+        endpoint = "videos.json"
+        params = {'clips.bioguide_ids': bioguide_id}
+        results = super(HouseVideos, cls)._apicall(endpoint, sections, make_obj, **params)
+        ### Only include clips from each video that contain a legistrators name
+        # Make sure results set has the legislators name sin it
+        videos = []
+        for v in results['videos']:
+            # Json examples in the documentation say there will be a 
+            # legislator_names however in practice they are not there
+            #if v.has_key('bioguide_id') and name in v['bioguide_id']:
+            clips = []
+            for c in v['clips']:
+                if c.has_key('bioguide_ids') and bioguide_id in c['bioguide_ids']:
+                    clips.append(c)
+
+            if len(clips) > 0:
+                v['clips'] = clips
+                videos.append(v)
+
         return videos
 
 class Amendments(RTC_Client):
--- tests/test_videos.py
+++ tests/test_videos.py
@@ -2,23 +2,38 @@ import RTC
 bill_id = 'hr2-112'
 RTC.apikey = 'c448541518f24d79b652ccc57b384815'
 leg_name = 'Mr. Poe of TX'
+bioguide_id = 'P000592'
+
+
 def test_by_bill():
-    videos = RTC.Videos.get_by_bill(bill_id)
+    videos = RTC.HouseVideos.get_by_bill(bill_id)
     assert len(videos) != 0, 'We expect at least one video to be returned'
     for v in videos:
         assert bill_id in v['bills'], 'The bill id that is being searched ' +\
                 'for should be in list of bills that are returned'
 
-    videos = RTC.Videos.get_by_bill('hrnotabill')
+    videos = RTC.HouseVideos.get_by_bill('hrnotabill')
     assert len(videos) == 0, 'not bills should match this bill id'
         
 def test_by_legislator_name():
-    results = RTC.Videos.get_legislator_name(leg_name)
+    results = RTC.HouseVideos.get_by_legislator_name(leg_name)
     assert len(results) != 0, 'We expect at least one video to be returned'
     for r in results:
         for c in r['clips']:
             assert leg_name in c['legislator_names'], 'Expecited name to ' +\
                     'be in legislator_names %s' % leg_name
         
-    results = RTC.Videos.get_legislator_name('not a legislator name')
+    results = RTC.HouseVideos.get_by_legislator_name('not a legislator name')
     assert len(results) == 0, 'there should be no legislators with this name'
+        
+def test_by_bioguide_id():
+    results = RTC.HouseVideos.get_by_bioguide_id(bioguide_id)
+    assert len(results) != 0, 'We expect at least one video to be returned'
+    for r in results:
+        for c in r['clips']:
+            assert bioguide_id in c['bioguide_ids'], 'Expecited bioguide_id to ' +\
+                    'be in bioguide_ids %s' % bioguide_id
+        
+    results = RTC.HouseVideos.get_by_bioguide_id('nobioguididhere')
+    assert len(results) == 0, 'there should be no legislators with this bioguide_id'
+
