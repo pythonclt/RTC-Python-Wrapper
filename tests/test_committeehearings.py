ad6326c1661e7e23cfba8a17a965139c527bb3ab
Matt Johnson <johnson.matthew.h@gmail.com>
2011-11-01 20:29:49 -0400

    fixed some tests to run off of static json files

--- tests/test_committeehearings.py
+++ tests/test_committeehearings.py
@@ -23,17 +23,21 @@ class TestCommitteeHearings(unittest.TestCase):
             assert self.query in i['description']
 
     def test_committeehearings_get(self):
-        #json path
-        #
+        json_path = 'CommitteeHearings/get.json'
         collection = RTC.CommitteeHearings
+        TestCommitteeHearings = alter_response(json_path, collection)
         self.date = '2011-10-06'
         self.chamber = 'senate'
         self.committee_id = 'SSEV'
         self.query = 'uranium'
-
-        self.committeehearings = RTC.CommitteeHearings.get(date=self.date, 
+         
+        self.committeehearings = TestCommitteeHearings.get(date=self.date, 
         chamber=self.chamber, committee_id=self.committee_id,
         search=self.query)
+
+        #self.committeehearings = RTC.CommitteeHearings.get(date=self.date, 
+        #chamber=self.chamber, committee_id=self.committee_id,
+        #search=self.query)
 
         assert len(self.committeehearings) !=0, 'There should be at least 1 result'
         #check that it contains the keyword arguments
