b41060a5bce6238da7edb6a494e17621bb9fdb82
Flaviu Simihaian <flsimihaian@gmail.com>
2012-03-27 19:06:11 -0400

    better setup.py?

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
-
 from distutils.core import setup
-from south import __version__
+from setuptools import find_packages
 
 setup(
-    name='RTC-Python-Wrapper',
-    version=__version__,
+    name='realtime_congress',
     description='Real Time Congress API Python Wrapper',
     long_description='Python wrapper for the Sunlight Foundation\'s Real Time Congress API.',
     author='Charlotte Python Group',
     author_email='flaviu@govkick.com',
     url='http://govkick.com/',
+    packages=find_packages(),
 )
