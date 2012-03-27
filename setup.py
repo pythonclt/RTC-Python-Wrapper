a103db3bce4590915846b82b7750194cc94628f9
Matt Johnson <johnson.matthew.h@gmail.com>
2012-03-27 19:54:11 -0400

    changed name of package in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='realtime_congress',
+    name='RTC',
     description='Real Time Congress API Python Wrapper',
     long_description='Python wrapper for the Sunlight Foundation\'s Real Time Congress API.',
     author='Charlotte Python Group',
