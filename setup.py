fd086ea5ce657b7aabe7450935c785edd97f5b30
Flaviu Simihaian <flsimihaian@gmail.com>
2012-03-27 16:55:15 -0400

    added setup.py to be installed for easy usage

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+from south import __version__
+
+setup(
+    name='RTC-Python-Wrapper',
+    version=__version__,
+    description='Real Time Congress API Python Wrapper',
+    long_description='Python wrapper for the Sunlight Foundation\'s Real Time Congress API.',
+    author='Charlotte Python Group',
+    author_email='flaviu@govkick.com',
+    url='http://govkick.com/',
+)
